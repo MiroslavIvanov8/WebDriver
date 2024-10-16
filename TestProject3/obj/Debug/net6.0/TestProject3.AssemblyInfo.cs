1d4b0751c0b2491717419fd89cf5df167bf61d85
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-10-17 00:32:05 +0300

    added jenkins file

--- TestProject3/obj/Debug/net6.0/TestProject3.AssemblyInfo.cs
+++ TestProject3/obj/Debug/net6.0/TestProject3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ce554a1d1dcedf9016f308b3493c0d8e60bf745")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f575e088d22543105a18a51486ece70b2f8c949")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
